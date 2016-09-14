10a3016a8d575c55f84dba009acde0212c81978d
Laura Allen <laurasellers@gmail.com>
2016-09-14 17:39:52 -0600

    add tests and functions for tile movement

--- test/tile-test.js
+++ test/tile-test.js
@@ -28,16 +28,33 @@ describe('Tile', function() {
 
     it('"moveLeft()" should decrease the "x" property by 1', function() {
       tile.moveLeft();
-      assert.equal(tile.x, 0)
+      assert.equal(tile.x, 0);
     });
 
     it('Should have a method called "moveUp()"', function() {
       assert.isFunction(tile.moveUp);
     });
 
+    it('"moveUp()" should increase the "y" property by 1', function() {
+      tile.moveUp();
+      assert.equal(tile.y, 1);
+    });
+
     it('Should have a method called "moveDown()"', function() {
       assert.isFunction(tile.moveDown);
     });
 
+    it('"moveDown()" should decrease the "y" property by 1', function() {
+      tile.moveDown();
+      assert.equal(tile.y, 0);
+    });
+
+    it('should have a method called "checkAdjacent()"', function() {
+      assert.isFunction(tile.checkAdjacent);
+    });
+
+    it('should check to see if there is an adjacent block', function() {
+
+    });
   });
 });
