e7f22c915d901d5ce239cec0d49394ff71e99fca
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-14 15:09:48 -0600

    First test of the test

--- main.bundle.js
+++ main.bundle.js
@@ -44,13 +44,31 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	'use strict';
+	// require('./index.js')
 
-	var sayHello = function sayHello() {
-	  return console.log('Hello');
-	};
+	function Tile(x, y, height, width) {
+	  this.x = x;
+	  this.y = y;
+	  this.height = 100;
+	  this.width = 100;
+	}
 
-	sayHello();
+	// function moveRight() {};
+	//
+	// function moveLeft() {};
+	//
+	// function moveUp() {};
+	//
+	// function moveDown() {};
+
+	// if the Tile has a position of 0,y it can't move left.
+	// if the Tile has a position of 3,y it can't move right.
+	// if the Tile has a position of x,0 it can't move up.
+	// if the Tile has a position of x,3 it can't move down.
+
+	// the Tile will have a tileValue property
+	// The Tile  will have default tileValue of 2.
+	//
 
 /***/ }
 /******/ ]);
--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 const assert = require('chai').assert;
 
+// const index = require('../lib/index.js');
+
 described('Tile', function() {
   context('With default attributes', function() {
-    it('Should assign default coordinates and size.', function() {
+    it.skip('Should assign default coordinates and size.', function() {
       var tile = new Tile(x, y, height, width);
       assert.equal(tile.x, 0);
       assert.equal(tile.y, 0);
