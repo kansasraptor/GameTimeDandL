544ba695c38abe1758b9ab09c129ce06a51bff52
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-14 17:21:54 -0600

    Initial move right and left functions written with tests

--- test/tile-test.js
+++ test/tile-test.js
@@ -4,12 +4,40 @@ const Tile = require('../lib/tile.js');
 
 describe('Tile', function() {
   context('With default attributes', function() {
+
+    var tile = new Tile(0, 0, 0)
+
     it('Should assign default coordinates and value.', function() {
-      var tile = new Tile(0, 0, 0)
       assert.equal(tile.x, 0);
       assert.equal(tile.y, 0);
       assert.equal(tile.tileValue, 2);
-
     });
+
+    it('Should have a method called "moveRight()"', function() {
+      assert.isFunction(tile.moveRight);
+    });
+
+    it('"moveRight()" should increase the "x" property by 1', function() {
+      tile.moveRight();
+      assert.equal(tile.x, 1);
+    });
+
+    it('Should have a method called "moveLeft()"', function() {
+      assert.isFunction(tile.moveLeft);
+    });
+
+    it('"moveLeft()" should decrease the "x" property by 1', function() {
+      tile.moveLeft();
+      assert.equal(tile.x, 0)
+    });
+
+    it('Should have a method called "moveUp()"', function() {
+      assert.isFunction(tile.moveUp);
+    });
+
+    it('Should have a method called "moveDown()"', function() {
+      assert.isFunction(tile.moveDown);
+    });
+
   });
 });
