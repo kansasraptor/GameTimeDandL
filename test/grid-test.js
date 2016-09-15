6fd63cf708fe1b4751c8ccaacb58da4148cc9aa1
Laura Allen <laurasellers@gmail.com>
2016-09-15 17:43:26 -0600

    constructs grid

--- test/grid-test.js
+++ test/grid-test.js
@@ -2,62 +2,15 @@ const assert = require('chai').assert;
 
 const Grid = require('../lib/grid.js');
 
-// var grid = new Grid();
+describe('Grid', function () {
 
-describe('Grid', function() {
-  // context('With default properties', function() {
+it('shoud be a function', function() {
+  assert.isFunction(Grid);
+});
 
-    it('Should be a function', function() {
-      assert.isFunction(Grid);
-  });
-    it('Should instantiate an object', function() {
-      var grid = new Grid();
-      assert.isObject(grid);
-    });
-
-    it('Should take the first arguement and set it as the rows property of the instantiated object', function() {
-      var grid = new Grid(4);
-      assert.equal(grid.row, 4);
-    });
-
-    it('should take the second arguement and set it as the columns propert of the instantiated object', function() {
-      var grid = new Grid(4, 4);
-      assert.equal(grid.column, 4);
-    });
-
-    it ('should have a "tiles" property, which starts out as an empty array', function() {
-      var grid = new Grid(4, 4);
-      assert.isArray(grid.tiles);
-      assert.deepEqual(grid.tiles, []);
-    });
-
-
-
-    
-
-    it.skip('should return the following sequence 0, 0, 0, 2',  );
-
-
-    it.skip('should determine the key value to determine move direction');
-
-    it.skip('should determine the starting position and direction of move from key value');
-
-    it.skip('should have a function that compares the "tileValue" properties of two tiles');
-
-    it.skip('should have a function that removes to tiles with the same "tileValue" property');
-
-    it.skip('should have a function that appends a new tile with the combined "tileValue" property of the removed tiles to the cell in the direction of the keypress key value');
-
-    it.skip('should have a function that slides tiles to chosen direction');
-
-
-
-    it.skip('should create an array of all cells with null values');
-
-    it.skip('should have a function that selects a random cell from the array of null values');
-
-    it.skip('should create a new tile that accepts the position of the random null cell');
-
-
+ it('should return an array', function() {
+   var grid = new Grid()
+   assert.isArray(Grid(4,4));
+ })
 
 });
